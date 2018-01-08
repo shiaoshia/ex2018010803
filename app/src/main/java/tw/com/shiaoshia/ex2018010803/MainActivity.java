1e7228a1914a71d2514011d12045df35b155d5c3
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2018-01-08 13:32:52 +0800

    BaseAdapter

---
+++ app/src/main/java/tw/com/shiaoshia/ex2018010803/MainActivity.java
@@ -0,0 +1,51 @@
+package tw.com.shiaoshia.ex2018010803;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv = (ListView)findViewById(R.id.listView);
+        myAdapter adapter = new myAdapter();
+        lv.setAdapter(adapter);
+
+    }
+
+    class myAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+
+            TextView tv = new TextView(MainActivity.this);
+            tv.setText("Hello" + position);
+            return tv;
+        }
+    }
+
+}
